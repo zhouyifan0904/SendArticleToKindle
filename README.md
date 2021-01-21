6f057ac0558aeabf31e4b978261a69598fa986d2
Max Zhou <zhouyifan0904@gmail.com>
2021-01-20 17:57:43 -0800

    added code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SendArticleToKindle
+
+## Usage:
+1. Clone the project
+2. Run `npm install`
+3. Set your email credentials in index.js, or create a separate file credentials.js to do that
+4. Run `node index.js <url>` to send an article
---
+++ index.js
@@ -0,0 +1,63 @@
+import Mercury from "@postlight/mercury-parser";
+import fs from "fs";
+import nodemailer from "nodemailer";
+import { displayName, email, password, targetEmail } from './credentials.js';
+
+
+if (!email) email = 'example@gmail.com';
+if (!password) password = '123456';
+if (!displayName) displayName = 'Bob';
+if (!targetEmail) targetEmail = 'example@kindle.com';
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: password
+  }
+});
+
+console.log("using url:");
+console.log(process.argv[2]);
+
+Mercury.parse(process.argv[2]).then((result) => {
+  if (result.error) {console.log("error parsing:"); return console.log(result.message)};
+
+  console.log("mercury parsed;");
+
+  let fn = `${result.title.replace(/[\W_]+/g, "_")}.html`;
+  let fp = `${process.argv[1].replace("\\index.js", "")}/archive/${fn}`;
+
+  let content = `<!DOCTYPE html><html><head><title>${result.title}</title><meta name="author" content="${result.author}"></head><body>`;
+  // remove images and links
+  content += result.content.replace(/<img[^>]*>|<a[^>]*>|<\/a>/gm, "");
+  content += `</body></html>`;
+   
+  fs.writeFile(fp, content, function (err) {
+    if (err) {console.log("error writing file:"); return console.log(err);}
+    console.log("Saved!");
+    
+    let mailOptions = {
+      from: `"${displayName}}" ${email}`,
+      to: targetEmail,
+      subject: 'convert',
+      text: "Hello! Here's an article for you.",
+      attachments: [
+        {
+          filename: fn,
+          path: fp
+        }
+      ]
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log("error sending email:");
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  
+  });
+});
